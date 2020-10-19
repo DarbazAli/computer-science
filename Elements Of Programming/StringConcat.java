feb2136374bf71651cc925488e98a52a334966c9
Darbaz Ali <darbaz.me@gmail.com>
2020-10-19 22:05:43 +0300

    Quadratic Equation

---
+++ Elements Of Programming/StringConcat.java
@@ -0,0 +1,7 @@
+public class StringConcat {
+    public static void main(String[] args) {
+        int age = 30;
+        Boolean maried = false;
+        System.out.println("Hi, my name is " + args[0] + ", and I'm " + age + " years old. But I'm " + maried);
+    }
+}
