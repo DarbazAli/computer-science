bc052033063f471984e01eb4a4cd50aa6a4c15ea
Darbaz Ali <darbaz.me@gmail.com>
2020-10-31 07:47:06 +0300

    refactor HN class

--- Elements Of Programming/ConditionalsAndLoops/HarmonicNumber.java
+++ Elements Of Programming/ConditionalsAndLoops/HarmonicNumber.java
@@ -4,13 +4,13 @@ public class HarmonicNumber {
     public static void main(String[] args) {
         // Compute the nth harmonic number
         int n = Integer.parseInt(args[0]);
-        double sum = 0.0;
+        double Hn = 0.0;
+
         for (int i = 1; i <= n; i++) {
-            // ...
-            sum += 1.0 / i;
+            Hn += 1.0 / i;
         }
 
-        // print the number
-        System.out.println(sum);
+        System.out.println(Hn);
+
     }
 }
