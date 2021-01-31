6e504a93eb2973f754ac722c4f6f3e3d0750a28a
Darbaz Ali <darbaz.me@gmail.com>
2021-01-31 16:57:10 +0300

    first java program

---
+++ Chapter_01_Elements_of_Programming/Section_01_First_Program/HelloWorld.java
@@ -0,0 +1,7 @@
+package Chapter_01_Elements_of_Programming.Section_01_First_Program;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
--- ElementsOfProgramming/FirstProgram/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public  class HelloWorld {
-    public  static void  main(String[] args) {
-        // print Hello, World to the console
-        System.out.println("Hello World");
-    }
-}
