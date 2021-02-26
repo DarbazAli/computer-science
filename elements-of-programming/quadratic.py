74d2c66db0e4fe5db28f4918751c662e3e9d0e0b
Darbaz Ali <darbaz.me@gmail.com>
2021-02-26 14:13:41 +0300

    float equality

--- elements-of-programming/quadratic.py
+++ elements-of-programming/quadratic.py
@@ -5,7 +5,10 @@ import sys
 b = float(sys.argv[1])
 c = float(sys.argv[2])
 
-discriminant = b * b - 4.0 * c
-d = math.sqrt(discriminant)
-print((-b + d) / 2.0)
-print((-b - d) / 2.0)
+if c == 0:
+    print('Error: division by zero!')
+else:
+    discriminant = b * b - 4.0 * c
+    d = math.sqrt(discriminant)
+    print((-b + d) / 2.0)
+    print((-b - d) / 2.0)
